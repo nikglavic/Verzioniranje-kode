2d4fe02b90fd0ca53eeaba4aa4be226f910b74cb
nikglavic <nik.glavic@gmail.com>
2023-04-02 18:17:32 +0200

    implemented my_prewitt function

--- 02_vaja_orv.py
+++ 02_vaja_orv.py
@@ -22,7 +22,21 @@ def my_roberts(slika):
     return slika_robov
 
 def my_prewitt(slika):
-    
+    slika_robov=np.zeros(slika.shape, dtype=np.uint8)
+    jedro1=np.array([[1,0, -1],[1, 0,-1], [1,0,-1]])
+    jedro2=np.array([[1,1,1],[0,0,0], [-1,-1,-1]])
+
+    maska1=np.zeros(slika.shape[:2])
+    maska2=np.zeros(slika.shape[:2])
+
+    for i in range(0, slika.shape[0]-2):
+        for j in range(0, slika.shape[1]-2):
+            del_slike=slika[i:i+3, j:j+3]
+            maska1[i,j]=np.sum(del_slike*jedro1)
+            maska2[i,j]=np.sum(del_slike*jedro2)
+    slika_robov=np.sqrt(np.square(maska1)+np.square(maska2))
+    slika_robov=np.uint8(slika_robov)
+
     return slika_robov 
 
 def my_sobel(slika):
@@ -36,6 +50,6 @@ def canny(slika, sp_prag, zg_prag):
 imgGray = cv2.imread('lenna.png',0)
 
 cv2.namedWindow("Slika")
-cv2.imshow("Slika", my_roberts(imgGray))
+cv2.imshow("Slika", my_prewitt(imgGray))
 cv2.waitKey()
 cv2.destroyAllWindows()
