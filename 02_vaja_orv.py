f64d4770731e37cafc270436815c42f485a33097
nikglavic <nik.glavic@gmail.com>
2023-04-02 17:50:56 +0200

    added canny defenition

--- 02_vaja_orv.py
+++ 02_vaja_orv.py
@@ -15,6 +15,10 @@ def my_sobel(slika):
     #vaša implementacija
     return slika_robov 
 
+def canny(slika, sp_prag, zg_prag):
+    #vaša implementacija
+    return slika_robov 
+
 imgGray = cv2.imread('lenna.png',0)
 
 cv2.namedWindow("Slika")
